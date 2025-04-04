d52533d7257c27439eff8d2de318a1a6acf86196
pedro.lima350 <pedro.lima350@etec.sp.gov.br>
2025-04-03 21:59:23 -0300

    TouchableOpacity

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,26 @@
+import CustomButtom from "@/components/Title";
 import React, { useState } from "react"
 import { Button, Text, View } from "react-native"
 
 export default function HomeScreen() {
+    const STARTVALUE = 0;
     const [count, setCount] = useState(0);
+
+    function alterarCount(value : number){
+        setCount(count + value);
+    }
 
     return (
         <View>
             <Text>opa</Text>
             <Text>Contamento: {count}</Text>
-            <Button title = "Aumente" onPress={() => {setCount(count + 1)}}/>
-            <Button title = "Diminua" onPress={() => {setCount(count - 1)}}/>
-            <Button title = "Reset" onPress={() => {setCount(0)}}/>
+            <Button title = "Aumente" onPress={() => {alterarCount(1)}}
+            />
+            <Button title = "Diminua" onPress={() => {alterarCount(-1)}}
+            />
+            <Button title = "Reset" onPress={() => {setCount(STARTVALUE)}}
+            />
+            <CustomButtom title="Real"/>
         </View>
 
     );
