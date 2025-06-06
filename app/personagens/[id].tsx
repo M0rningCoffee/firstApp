84895b7f225e7b51589374f88c4b8c944bb01eb2
pedro.lima350 <pedro.lima350@etec.sp.gov.br>
2025-06-05 22:13:41 -0300

    UseEffect

--- app/personagens/[id].tsx
+++ app/personagens/[id].tsx
@@ -1,9 +1,15 @@
 import { useLocalSearchParams } from "expo-router";
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, Text, View } from "react-native";
 
 export default function Detail() {
     const {id} = useLocalSearchParams(); //Pega o id da url
+
+
+    useEffect(() =>{
+
+    })
+    
     return(
         <View>
             <Text>Detalhes: {id}</Text>
